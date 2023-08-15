ce89392b0f51996164be090ccb86a7e76d4d524e
Mohamed Salem Mohamed Ahmed <msalem@hq.ip.mr>
2023-08-15 16:04:07 +0200

    first commit

---
+++ src/test/java/com/salem/gestionstock/GestionDeStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salem.gestionstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
